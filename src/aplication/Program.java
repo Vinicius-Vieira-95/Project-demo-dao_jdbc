b2f7ab83a43274a2ccf26e0eec7fb9cfe9ec056f
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2020-08-31 11:07:27 -0300

    Adcionado classe Seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,11 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 		System.out.println(obj);
+		
 		
 	}
 
