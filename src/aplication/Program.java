4eeb4facc856cd22142cdce1223b20017c75e401
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2020-09-01 11:56:11 -0300

    Implementado metodo findByDepartment

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,7 +1,10 @@
 package aplication;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,11 +14,17 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
+		System.out.println("=== Teste 1 FindById =======");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
+		System.out.println("=== Teste 2 SellerFindByDepartment =======");
+		Department dep = new Department(2, null);
+		List<Seller>list = sellerDao.findByDepartment(dep);
 		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
