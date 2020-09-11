7f8c36072c3946105234fbdd4be03956e364bf3e
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2020-09-11 09:09:48 -0300

    Adcionando classe DepartmentDaoJDBC e implementado metodo creatDepartmentDao na classe DaoFactory

---
+++ src/aplication/Program2.java
@@ -0,0 +1,9 @@
+package aplication;
+
+public class Program2 {
+
+	public Program2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,9 @@
+package model.dao.impl;
+
+public class DepartmentDaoJDBC {
+
+	public DepartmentDaoJDBC() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
